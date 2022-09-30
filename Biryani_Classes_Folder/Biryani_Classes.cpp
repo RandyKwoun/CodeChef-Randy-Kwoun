f582392e3ba4c753437401d7572b47daeb897080
RandyKwoun <114547123+RandyKwoun@users.noreply.github.com>
2022-09-30 13:11:20 -0700

    Add files via upload

---
+++ Biryani_Classes_Folder/Biryani_Classes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int iT;
+
+    cin >> iT;
+
+    while(iT--)
+    {
+        int iNum1, iNum2;
+
+        cin >> iNum1 >> iNum2;
+
+        cout << iNum1 * iNum2 << endl;
+    }
+
+    return 0;
+}
