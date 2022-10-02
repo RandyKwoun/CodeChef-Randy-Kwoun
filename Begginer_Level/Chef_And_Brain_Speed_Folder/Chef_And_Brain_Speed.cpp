046ab61f6cc021a319a17f8b1243accbb39addd6
RandyKwoun <114547123+RandyKwoun@users.noreply.github.com>
2022-10-01 17:31:13 -0700

    Add files via upload

---
+++ Begginer_Level/Chef_And_Brain_Speed_Folder/Chef_And_Brain_Speed.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int iNum1, iNum2;
+
+    cin >> iNum1 >> iNum2;
+
+    if(iNum1 >= iNum2)
+        cout << "NO" << endl;
+
+    else    
+        cout << "YES" << endl;
+
+    return 0;
+}
